472ae3f816d25219c58b6c630d1ef6d0712fc894
krushovice <kickstar69@yandex.ru>
2023-05-09 17:55:19 +1000

    build func

--- gendif/diff.py
+++ gendif/diff.py
@@ -1,6 +1,17 @@
 import json
 
 
+def check_format(array):
+    for key in array:
+        if array[key] is True:
+            array[key] = 'true'
+        elif array[key] is False:
+            array[key] = 'false'
+        elif array[key] is None:
+            array[key] = 'null'
+    return array
+
+
 def generate_diff(file_path1, file_path2):
     with open(file_path1, 'r') as f1, open(file_path2, 'r') as f2:
         json1 = json.load(f1)
@@ -12,25 +23,29 @@ def generate_diff(file_path1, file_path2):
 
     for key in json2.keys():
         output[key] = json2[key]
+    output = check_format(output)
 
     def to_string():
         sorted_keys = sorted(output.keys())
-        result = []
+        my_list = ['{']
         for key in sorted_keys:
             value = output[key]
             if key in json1.keys():
                 if key not in json2.keys():
-                    result.append(f'- {key}: {value}')
+                    my_list.append(f'- {key}: {value}')
             if key in json2.keys():
                 if key not in json1.keys():
-                    result.append(f'+ {key}: {value}')
+                    my_list.append(f'+ {key}: {value}')
             if key in json1.keys() & json2.keys():
                 if json1[key] != json2[key]:
-                    result.append(f'- {key}: {json1[key]}')
-                    result.append(f'+ {key}: {json2[key]}')
+                    my_list.append(f'- {key}: {json1[key]}')
+                    my_list.append(f'+ {key}: {json2[key]}')
 
                 else:
-                    result.append(f'{key}: {value}')
-        return result
+                    my_list.append(f'{key}: {value}')
+        my_list.append('}')
 
+        result = "\n".join(my_list)
+
+        return result
     return to_string()
--- gendif/scripts/gendiff.py
+++ gendif/scripts/gendiff.py
@@ -1,4 +1,3 @@
-import json
 from gendif.diff import generate_diff
 from gendif.parser import start_argparse
 
@@ -6,7 +5,7 @@ from gendif.parser import start_argparse
 def main():
     diff = generate_diff('gendif/file1.json', 'gendif/file2.json')
     print(start_argparse())
-    print(json.dumps(diff, indent=2))
+    print(diff)
 
 
 if __name__ == '__main__':
