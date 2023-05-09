197dc23fb5ae534838ea8edd56b723f4cadaa633
krushovice <kickstar69@yandex.ru>
2023-05-09 15:46:41 +1000

    add parser

--- gendif/scripts/gendiff.py
+++ gendif/scripts/gendiff.py
@@ -1,23 +1,13 @@
-import argparse
 import json
 from gendif.diff import generate_diff
+from parser import start_argparse
 
-parser = argparse.ArgumentParser(
-                    prog='gendiff',
-                    description="""Compares two configuration
-                    files and shows a difference."""
-                    )
-# Позиционные аргументы
-parser.add_argument('first_file')
-parser.add_argument('second_file')
 
-# optional, именованные аргументы
-parser.add_argument('-f', '--format', help='set format of output')
-
-args = parser.parse_args()
-print(args)
+def main():
+    diff = generate_diff('gendif/file1.json', 'gendif/file2.json')
+    print(start_argparse())
+    print(json.dumps(diff, indent=2))
 
 
 if __name__ == '__main__':
-    diff = generate_diff('gendif/file1.json', 'gendif/file2.json')
-    print(json.dumps(diff, indent=2))
+    main()
