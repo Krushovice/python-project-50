474570ab4522de7c126b704484a3b1e182d7dcad
krushovice <kickstar69@yandex.ru>
2023-05-09 15:06:47 +1000

    add format func

--- gendif/diff.py
+++ gendif/diff.py
@@ -8,25 +8,29 @@ def generate_diff(file_path1, file_path2):
 
     output = {}
     for key in json1.keys():
-        if key not in json2:
-            output[key] = {
-                '- ' + key: json1[key]
-            }
+        output[key] = json1[key]
 
     for key in json2.keys():
-        if key not in json1:
-            output[key] = {
-                '+ ' + key: json2[key]
-            }
+        output[key] = json2[key]
 
-    for key in json1.keys() & json2.keys():
-        if json1[key] != json2[key]:
-            output[key] = {
-                '- ' + key: json1[key],
-                '+ ' + key: json2[key]
-            }
-        else:
-            output[key] = {
-                key: json1[key]
-            }
-    return output
+    def to_string():
+        sorted_keys = sorted(output.keys())
+        result = []
+        for key in sorted_keys:
+            value = output[key]
+            if key in json1.keys():
+                if key not in json2.keys():
+                    result.append(f'- {key}: {value}')
+            if key in json2.keys():
+                if key not in json1.keys():
+                    result.append(f'+ {key}: {value}')
+            if key in json1.keys() & json2.keys():
+                if json1[key] != json2[key]:
+                    result.append(f'- {key}: {json1[key]}')
+                    result.append(f'+ {key}: {json2[key]}')
+
+                else:
+                    result.append(f'{key}: {value}')
+        return result
+
+    return to_string()
