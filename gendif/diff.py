9cdf474e069e08bc5f35c8cd56434bfd693caf6b
krushovice <kickstar69@yandex.ru>
2023-05-15 17:30:09 +1000

    fix config

--- gendif/diff.py
+++ gendif/diff.py
@@ -1,15 +1,20 @@
 import json
 
 
-def check_format(array):
-    for key in array:
-        if array[key] is True:
-            array[key] = 'true'
-        elif array[key] is False:
-            array[key] = 'false'
-        elif array[key] is None:
-            array[key] = 'null'
-    return array
+def check_format(value):
+    if value is True:
+        return 'true'
+    elif value is False:
+        return 'false'
+    elif value is None:
+        return 'null'
+    return value
+
+
+def get_diff_entry(key, value1, value2):
+    if value1 != value2:
+        return [f'- {key}: {value1}', f'+ {key}: {value2}']
+    return [f'  {key}: {value1}']
 
 
 def generate_diff(file_path1, file_path2):
@@ -19,33 +24,60 @@ def generate_diff(file_path1, file_path2):
 
     output = {}
     for key in json1.keys():
-        output[key] = json1[key]
+        output[key] = check_format(json1[key])
 
     for key in json2.keys():
-        output[key] = json2[key]
-    output = check_format(output)
+        if key not in output:
+            output[key] = check_format(json2[key])
 
-    def to_string():
-        sorted_keys = sorted(output.keys())
-        my_list = ['{']
-        for key in sorted_keys:
-            value = output[key]
-            if key in json1.keys():
-                if key not in json2.keys():
-                    my_list.append(f'- {key}: {value}')
-            if key in json2.keys():
-                if key not in json1.keys():
-                    my_list.append(f'+ {key}: {value}')
-            if key in json1.keys() & json2.keys():
-                if json1[key] != json2[key]:
-                    my_list.append(f'- {key}: {json1[key]}')
-                    my_list.append(f'+ {key}: {json2[key]}')
+    sorted_keys = sorted(output.keys())
+    result = ['{']
+    for key in sorted_keys:
+        if key in json1 and key in json2:
+            result.extend(get_diff_entry(key, output[key], json2[key]))
+        elif key in json1:
+            result.append(f'- {key}: {output[key]}')
+        elif key in json2:
+            result.append(f'+ {key}: {output[key]}')
 
-                else:
-                    my_list.append(f'{key}: {value}')
-        my_list.append('}')
+    result.append('}')
 
-        result = "\n".join(my_list)
+    return '\n'.join(result)
 
-        return result
-    return to_string()
+# def generate_diff(file_path1, file_path2):
+#     with open(file_path1, 'r') as f1, open(file_path2, 'r') as f2:
+#         json1 = json.load(f1)
+#         json2 = json.load(f2)
+
+#     output = {}
+#     for key in json1.keys():
+#         output[key] = json1[key]
+
+#     for key in json2.keys():
+#         output[key] = json2[key]
+#     output = check_format(output)
+
+#     def to_string():
+#         sorted_keys = sorted(output.keys())
+#         my_list = ['{']
+#         for key in sorted_keys:
+#             value = output[key]
+#             if key in json1.keys():
+#                 if key not in json2.keys():
+#                     my_list.append(f'- {key}: {value}')
+#             if key in json2.keys():
+#                 if key not in json1.keys():
+#                     my_list.append(f'+ {key}: {value}')
+#             if key in json1.keys() & json2.keys():
+#                 if json1[key] != json2[key]:
+#                     my_list.append(f'- {key}: {json1[key]}')
+#                     my_list.append(f'+ {key}: {json2[key]}')
+
+#                 else:
+#                     my_list.append(f'{key}: {value}')
+#         my_list.append('}')
+
+#         result = "\n".join(my_list)
+
+#         return result
+#     return to_string()
